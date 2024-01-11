5bf297488a18b4970e1f0214c0cc673d99d6c4f2
Lucas Mauricio <lucasmauriciofc@gmail.com>
2024-01-11 09:30:08 -0300

    feat: service and controller created

---
+++ devbills-api/src/services/categories.service.ts
@@ -0,0 +1,12 @@
+import { Category } from '../entites/category.entity'
+
+export class CategoriesService {
+  async create(): Promise<Category> {
+    const category = new Category({
+      title: 'Example Category',
+      color: '#ff33bb',
+    })
+
+    return category
+  }
+}
